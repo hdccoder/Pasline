5cdd9f77a883b4ced5cfb1001fca1c5f281eee87
Heather Lockett <heatherlockett@Heathers-MacBook-Pro.local>
2024-03-18 05:19:52 -0400

    cont'd

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,12 +5,10 @@ import {
   Button,
   IconButton,
   Toolbar,
-  Tooltip,
   Typography,
-  Menu,
+  Popover,
   MenuItem,
 } from '@mui/material';
-import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
@@ -45,75 +43,79 @@ const Header = () => {
   };
 
   return (
-    <AppBar position="absolute" sx={{ backgroundColor: 'white', height: '10rem' }}>
+    <AppBar position="absolute" sx={{ backgroundColor: 'white', height: '10rem', }}>
       <Toolbar>
-        <Button sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box
-            onClick={() => navigate('/')}
-            component="img"
-            sx={{
+        <Button onClick={() => navigate('/')} color="inherit">
+          <img
+            src="/public/assets/PLOGO5.png"
+            alt="pic of a record"
+            style={{
               height: '58rem',
               width: '60rem',
               paddingTop: '50px',
               marginLeft: '-300px',
               marginTop: '-40px',
             }}
-            alt="pic of a record"
-            src="/public/assets/PLOGO5.png"
           />
         </Button>
-        <Tooltip title="Home">
-          <IconButton
-            color="inherit"
-            aria-label="Home"
-            onClick={() => navigate('/')}
-            sx={{ marginRight: 10, marginBottom: -8, marginLeft: -22, color: '#0319bd' }}
-          >
-            <Typography variant="h4">HOME</Typography>
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="About Us">
-          <IconButton
-            color="inherit"
-            aria-label="About Us"
-            onClick={handleFaqMenuOpen}
-            sx={{ marginRight: 10, marginBottom: -8, marginLeft: -8, color: '#0319bd' }}
-          >
-            <Typography variant="h4">ABOUT US</Typography>
-          </IconButton>
-        </Tooltip>
-        <Menu
-          id="faq-menu"
-          anchorEl={faqAnchorEl}
-          open={Boolean(faqAnchorEl)}
-          onClose={handleFaqMenuClose}
+        <Button
+          onClick={() => navigate('/')}
+          color="inherit"
+          variant="text"
+          sx={{ color: '#0319bd', marginLeft: '-9.2rem', marginBottom: '-80', }}
         >
-          <MenuItem onClick={handleFaqClick}>FAQ</MenuItem>
-        </Menu>
-        <Box>
-          <Tooltip title="Our Services">
-            <IconButton
-              color="inherit"
-              aria-label="Our Services"
-              onClick={handleServicesMenuOpen}
-              sx={{ marginBottom: -8, marginLeft: -8, color: '#0319bd' }}
-            >
-              <Typography variant="h4">OUR SERVICES</Typography>
-            </IconButton>
-          </Tooltip>
-          <Menu
-            id="services-menu"
-            anchorEl={servicesAnchorEl}
+          <Typography variant="h4">HOME</Typography>
+        </Button>
+        <Typography
+          variant="h4"
+          sx={{ color: '#0319bd', marginLeft: '2rem', marginBottom: '-80',}}
+          onMouseEnter={handleFaqMenuOpen}
+          onMouseLeave={handleFaqMenuClose}
+        >
+          ABOUT US
+          <Popover
+            open={Boolean(faqAnchorEl)}
+            anchorEl={faqAnchorEl}
+            onClose={handleFaqMenuClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <MenuItem onClick={handleFaqClick}>FAQ</MenuItem>
+          </Popover>
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{ color: '#0319bd', marginLeft: '2rem', marginBottom: '-80', }}
+          onMouseEnter={handleServicesMenuOpen}
+          onMouseLeave={handleServicesMenuClose}
+        >
+          OUR SERVICES
+          <Popover
             open={Boolean(servicesAnchorEl)}
+            anchorEl={servicesAnchorEl}
             onClose={handleServicesMenuClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
           >
             <MenuItem onClick={() => handleServiceClick('care-and-companionship')}>CARE & COMPANIONSHIP</MenuItem>
             <MenuItem onClick={() => handleServiceClick('memory-care')}>MEMORY CARE</MenuItem>
             <MenuItem onClick={() => handleServiceClick('hospital-to-home-care')}>HOSPITAL TO HOME CARE</MenuItem>
             <MenuItem onClick={() => handleServiceClick('disability')}>DISABILITY</MenuItem>
             <MenuItem onClick={() => handleServiceClick('specialized-care')}>SPECIALIZED CARE</MenuItem>
-          </Menu>
-        </Box>
+          </Popover>
+        </Typography>
       </Toolbar>
     </AppBar>
   );
